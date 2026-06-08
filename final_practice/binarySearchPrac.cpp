abb0a838be0bb0f9a6103cd23f6c127df1b91a7a
syed jawad <jawadabdullah730@gmail.com>
2026-06-08 23:59:59 +0500

    practice

---
+++ final_practice/binarySearchPrac.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool binarySearch(int key,int arr[],int size){
+    int start = 0,end = size-1;
+    int mid = (start+end)/2;
+    while (start<=end)
+    {
+        if (key==arr[mid])
+        {
+            return true;
+        }
+        else if(key>arr[mid]){
+            start = mid + 1;
+        }
+        else if (key<arr[mid])
+        {
+            end = mid - 1;
+        }
+    }
+    return false;
+}
+
+int main(){
+    int arr[] = {10, 20, 30, 40, 50, 60, 70};
+    cout << binarySearch(40,arr,7);
+}
